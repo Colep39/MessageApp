d53485f10d39d668276b0b7291a141c8121fd977
Cole Plagens <142531035+Colep39@users.noreply.github.com>
2025-08-05 23:15:02 -0500

    initialized prisma orm psql database with user and message relations

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,12 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the backend!');
+});
+app.get('/favicon.ico', (req, res) => res.status(204)); // gets rid of dumb favicon error
+
+
 // Sample route
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
